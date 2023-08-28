0b43b1d278060142cbddfcea420d6521f4295fb3
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2023-08-28 02:04:26 +0100

    made further improvements to url encoding

--- hooks/useGetCartOrder.js
+++ hooks/useGetCartOrder.js
@@ -3,11 +3,11 @@ import { CartContext } from '@/components/Layout'
 
 export default function () {
   const { cart } = useContext(CartContext)
-  return `I would like to place an order for the following items:%0A
+  return `I would like to place an order for the following items:%0A%0A
 ${cart.map(
   ({ item, count }, index) =>
     `${index + 1}) ${
       item.name
-    }(quantity:${count}, url:${`https://pharmahubmedica.ng/collections/${item?.category.slug.current}/${item?.slug.current}`}%20)%0A`
+    }(quantity:${count}, url:${`https://pharmahubmedica.ng/collections/${item?.category.slug.current}/${item?.slug.current}`}%20)%0A%0A`
 )}`
 }
