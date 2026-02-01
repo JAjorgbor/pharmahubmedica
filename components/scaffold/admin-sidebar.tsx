ba1346380bb23b3548108d738cb7b980d5f915a9
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2026-02-01 01:06:44 +0100

    root domain setup for production back to store links

--- components/scaffold/admin-sidebar.tsx
+++ components/scaffold/admin-sidebar.tsx
@@ -31,6 +31,7 @@ import {
   LuUsers,
 } from 'react-icons/lu'
 import { useSidebarStore } from '@/stores/useSidebarStore'
+import { getDomain } from 'tldts'
 
 const AdminSidebar = () => {
   const isMobile = useMediaQuery('md')
@@ -38,6 +39,14 @@ const AdminSidebar = () => {
   const { openSidebar, setOpenSidebar } = useSidebarStore()
   const pathname = usePathname()
   const [isHydrated, setIsHydrated] = useState(false)
+
+  const [rootDomain, setRootDomain] = useState('/')
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setRootDomain(getDomain(window.location.hostname))
+    }
+  }, [])
 
   useEffect(() => {
     if (!isMobile && openSidebar) setOpenSidebar(false)
@@ -292,7 +301,7 @@ const AdminSidebar = () => {
           <MenuItem
             icon={<LuHouse size={20} />}
             className="text-foreground-600"
-            component={<Link href="/" />}
+            component={<Link href={rootDomain || '/'} />}
           >
             Back to Store
           </MenuItem>
